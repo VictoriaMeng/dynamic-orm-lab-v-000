b4d68be99e9282024defc8013e56ad180377b73a
Victoria Meng <nights1ststar@protonmail.com>
2018-05-03 00:23:31 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,27 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+
+    column_names = []
+
+    table_info.each { |column| column_names << column["name"] }
+
+    column_names.compact
+  end
+
+  self.column_names.each do |name|
+    attr_accessor name.to_sym
+  end
+
+  def initialize(attributes={})
+    attributes.each { |key, value| self.send("#{key}=", value) }
+  end
 end
