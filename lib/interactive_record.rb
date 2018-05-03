028196d8bbb0ee4a96fb1187b53bb9b5f71f29c7
Victoria Meng <nights1ststar@protonmail.com>
2018-05-03 00:38:24 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
+    column = self.column_names.find { |column| column == attribute.keys[0] }
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.key.to_s} = ?"
   end
 end
